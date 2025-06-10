cc1ca27ec58f65a959b6fb18ca6234263a2094ca
Humoyun Akbaraliev <136078810+humoyunswe@users.noreply.github.com>
2025-06-10 18:20:04 +0500

    from socket module created server with port

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+import socket
+
 def main():
-    pass
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept()
 
 if __name__ == "__main__":
     main()
