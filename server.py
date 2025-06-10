ff770399faf6e2f8411dcabe688b2ff5904756a1
Humoyun Akbaraliev <136078810+humoyunswe@users.noreply.github.com>
2025-06-10 19:17:31 +0500

    add create_response function which creates all HTTP requests in programme

--- server.py
+++ server.py
@@ -5,6 +5,19 @@ PORT = 4221
 files_directory = ""
 
 
+def create_response(status, content="", content_type="text/plain"):
+    """
+    Creates HTTP response from parts:
+        - handle_home()
+        - handle_echo()
+        - handle_user_agen()
+        - read_file()
+        - write_file()
+        - handle_files()
+        - route_request()
+        - handle_client()
+    """
+    
 
 
 def main():
